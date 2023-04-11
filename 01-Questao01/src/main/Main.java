4829a94451386a76d634aa5d2745caf99e48e75b
Gabriel Ribeiro <gaabriel.jrb@gmail.com>
2023-04-11 09:50:28 -0300

    Question 1 fix and question 2

--- 01-Questao01/src/main/Main.java
+++ 01-Questao01/src/main/Main.java
@@ -12,22 +12,21 @@ public class Main {
 		Scanner sc = new Scanner(System.in);
 
 		System.out.println("Digite a operacao que deseja fazer: ");
-		System.out.println("1: Adicao \n" + 
-		"2: Subtracao \n" +
-		"3: Divisao \n" + 
-		"4: Multiplicacao \n" +
-		"5: Raiz quadrada e raiz cubica\n" +
-		"6: Todas as operacoes");
-		
+		System.out.println("1: Adicao \n" + "2: Subtracao \n" + "3: Divisao \n" + "4: Multiplicacao \n"
+				+ "5: Raiz quadrada e raiz cubica\n" + "6: Todas as operacoes");
+
 		try {
 			Integer operacao = sc.nextInt();
-			
-			System.out.println("Digite dois numeros: ");
-			
-			double a = sc.nextDouble();
-			double b = sc.nextDouble();
-			
-			switch(operacao) {
+
+			if (operacao < 0 || operacao > 6) {
+				System.out.println("Entrada invalida");
+			} else {
+				System.out.println("Digite dois numeros: ");
+
+				double a = sc.nextDouble();
+				double b = sc.nextDouble();
+
+				switch (operacao) {
 				case 1:
 					Calculadora.calculaSoma(a, b);
 					break;
@@ -48,15 +47,17 @@ public class Main {
 					Calculadora.todasAsOperacoes(a, b);
 					break;
 				default:
-					System.out.println("Entrada invalida");
+					System.out.println("Entrada invalida da operacao");
 					break;
+				}
+
 			}
 		}
-		
+
 		catch (InputMismatchException e) {
 			throw new Excecao("Tipo errado");
 		}
-		
+
 	}
 
 }
